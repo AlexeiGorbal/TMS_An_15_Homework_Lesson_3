02caa5cf78a24530c03c15088b3e577e0059b192
lemon <lemon.juice9341@gmail.com>
2023-08-05 18:17:06 +0300

    In this class, a variable of type double is declared and explicitly cast to type int

---
+++ src/Exercise_4.java
@@ -0,0 +1,7 @@
+public class Exercise_4 {
+    public static void main(String[] args) {
+        double x1 = 152.25;
+        int x2 = (int) x1;
+        System.out.println("x1: " + x1 + "\nx2: " + x2);
+    }
+}
