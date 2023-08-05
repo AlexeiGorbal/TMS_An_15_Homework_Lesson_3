36c83d186542c65b19770f58eaa94765b428615f
lemon <lemon.juice9341@gmail.com>
2023-08-05 17:50:31 +0300

    Four variables of type int are declared in the class
    the first is in binary, the second is in octal, the third is in decimal, and the fourth is in hexadecimal.

---
+++ src/Exercise_2.java
@@ -0,0 +1,12 @@
+public class Exercise_2 {
+    public static void main(String[] args) {
+        int x1 = 0b110;
+        int x2 = 01347;
+        int x3 = 19223;
+        int x4 = 0x15A19;
+        System.out.println("Number in binary form: " + x1);
+        System.out.println("Number in octal form: " + x2);
+        System.out.println("Number in decimal form: " + x3);
+        System.out.println("Number in hexadecimal form: " + x4);
+    }
+}
