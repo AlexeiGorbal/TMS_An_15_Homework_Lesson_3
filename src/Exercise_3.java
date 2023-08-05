1788ae71a5596dc1201e29c3bb1d923ed1f7c7d8
lemon <lemon.juice9341@gmail.com>
2023-08-05 18:01:28 +0300

    This class declares two variable of type int
    the first is called by the increment support the second by the decriment support

---
+++ src/Exercise_3.java
@@ -0,0 +1,9 @@
+public class Exercise_3 {
+    public static void main(String[] args) {
+        int x1 = 15;
+        int x2 = 20;
+        x1++;
+        x2--;
+        System.out.println("x1: " + x1 + "\nx2: " + x2);
+    }
+}
