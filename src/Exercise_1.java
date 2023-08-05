960f1456956ac3398a6cc685a732639a68c0022c
lemon <lemon.juice9341@gmail.com>
2023-08-05 17:36:58 +0300

    In this class, the result of three variable is displayed.
    first arbitrary value
    the second is three times the first
    the third is the sum of the previous variables

---
+++ src/Exercise_1.java
@@ -0,0 +1,8 @@
+public class Exercise_1 {
+    public static void main(String[] args) {
+        int x1 = 15;
+        int x2 = 3 * x1;
+        int x3 = x1 + x2;
+        System.out.println("x1: " + x1 + "\nx2: " + x2 + "\nx3: " + x3);
+    }
+}
